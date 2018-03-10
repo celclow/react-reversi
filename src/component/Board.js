886e5905ddfb7364de7c96ffa01d5d685b340196
celclow <celclow@gmail.com>
2018-03-11 00:24:27 +0900

    ボードサイズ実装

--- src/component/Board.js
+++ src/component/Board.js
@@ -3,21 +3,26 @@ import ReactDOM from 'react-dom';
 import Disk from './Disk';
 
 class Board extends React.Component {
+
+
     constructor(props) {
         super(props);
+    }
+
+    static getBoardSize() {
+        return 8;
     }
 
     render() {
         return (
             <div className="board">
-                <Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk />
-                <Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk />
-                <Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk />
-                <Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk />
-                <Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk />
-                <Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk />
-                <Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk />
-                <Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk /><Disk />
+                {
+                    [...Array(Board.getBoardSize()).keys()].map(rownum => {
+                        return [...Array(Board.getBoardSize()).keys()].map(colnum => {
+                            return <Disk row={rownum} col={colnum} />
+                        })
+                    })
+                }
             </div>
         );
     }
