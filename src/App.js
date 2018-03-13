c686ba866899b50e264148354e9b82a18b6c3ac4
celclow <celclow@gmail.com>
2018-03-14 00:16:07 +0900

    不要なファイルを削除。

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Board from './component/Board';
 
@@ -8,8 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">reversu</h1>
         </header>
         <Board />
       </div>
